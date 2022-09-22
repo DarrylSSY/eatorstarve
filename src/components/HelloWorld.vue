57f3188dc97d306097e0c4b8801b3f9816121531
DarrylSSY <ssy.darryl@gmail.com>
2022-09-22 14:05:10 +0800

    Verify if socket.io is working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,8 +31,14 @@
 </template>
 
 <script>
+import {io} from 'socket.io-client'
 export default {
   name: 'HelloWorld',
+  data (){
+    return {
+      socket: io('http://localhost:4000', { transports : ['websocket'] })
+    }
+  },
   props: {
     msg: String
   }
