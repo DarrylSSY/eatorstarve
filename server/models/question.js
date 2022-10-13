336f572b0b2dce12b1c993b2f4297f8ab95c1d05
DarrylSSY <ssy.darryl@gmail.com>
2022-10-13 22:50:00 +0800

    test commit

--- server/models/question.js
+++ server/models/question.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const QuestionSchema = new mongoose.Schema({
     category: { type: String},
     answer: { type: String },
+    abc = 123,
 });
 
 module.exports = mongoose.model('Question', QuestionSchema);
