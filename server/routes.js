bfa96e85e226cf855e8e394fc86b333be99d30d4
DarrylSSY <ssy.darryl@gmail.com>
2022-11-03 13:36:17 +0800

    Deployment to Heroku

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 require('mongoose');
 const axios = require("axios");
 const Answer = require("./models/answer");
 const Question = require("./models/question");
 const CreatedRoom = require("./models/createdroom");
+
 // get list of rooms
 router.post('/places', (req, res) => {
     axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+req.body["parameters"]+'&maxprice='+req.body["maxprice"]+'&minprice='+req.body["minprice"]+'&key=AIzaSyCDluC6rpLOcgskAumfnCWAOdGrAE1bb5M&type=restaurant')
