3643ba6ef2597d3d8482781779bae4b6b7f268fe
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2022-11-09 15:45:28 +0800

    Add typewriter animation and cat

--- src/components/DialogueBox.vue
+++ src/components/DialogueBox.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="row gx-4">
-        <div class="nes-container col-12 box is-rounded">
-            {{text}}
+    <div class="box nes-container is-rounded">
+        <div class="row">
+            <div class="col-1">
+                <i class="nes-octocat animate"></i>
+            </div>
+            <div class="col-11 text">
+                <p class="typewriter">{{text}}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -16,7 +21,11 @@ export default {
         return {
             text: this.message
         }
-    }
+    },
+    
+    created() {
+    setTimeout(() => this.visible =  ``, 3500)
+    },
 }
 </script>
 
@@ -26,6 +35,33 @@ export default {
         width: 1300px;
         position: fixed;
         bottom: 0;
-        background-color: white;
     }
+
+    .text {
+        padding: 10px 30px 0px 30px;
+        /* text-align: center; */
+        align-items: center;
+    }
+    .typewriter {
+        overflow: hidden; /* Ensures the content is not revealed until the animation */
+        /* border-right: .15em solid orange; The typwriter cursor */
+        white-space: nowrap; /* Keeps the content on a single line */
+        /* margin: 0 auto; Gives that scrolling effect as the typing happens */
+        letter-spacing: .15em; /* Adjust as needed */
+        animation: 
+            typing 2.5s steps(40, end),
+            blink-caret .75s step-end infinite;
+    }
+
+    /* The typing effect */
+    @keyframes typing {
+        from { width: 0 }
+        to { width: 100% }
+    }
+
+    /* The typewriter cursor effect */
+    /* @keyframes blink-caret {
+        from, to { border-color: transparent }
+        50% { border-color: orange; }
+    } */
 </style>
